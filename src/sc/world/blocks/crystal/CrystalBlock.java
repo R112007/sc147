e0ba29b07841b93daee6164288b96bfad9a37829
R112007 <1828746771@qq.com>
2025-04-23 00:15:08 +0800

    sc147.7

---
+++ src/sc/world/blocks/crystal/CrystalBlock.java
@@ -0,0 +1,10 @@
+package sc.world.blocks.crystal;
+
+import mindustry.world.Block;
+
+public class CrystalBlock extends Block {
+  public CrystalBlock(String name) {
+    super(name);
+  }
+
+}
---
+++ src/sc/world/meta/SCBlockGroup.java
@@ -0,0 +1,6 @@
+package sc.world.meta;
+
+import mindustry.world.meta.BlockGroup;
+
+public class SCBlockGroup {
+}
