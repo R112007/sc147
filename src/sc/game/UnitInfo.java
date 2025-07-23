2b80f789ed1bc052f3ac7cd6b0a56aded6027c8f
R112007 <1828476771@qq.com>
2025-07-24 06:32:57 +0800

    e

---
+++ src/sc/game/UnitInfo.java
@@ -0,0 +1,62 @@
+package sc.game;
+
+import arc.math.WindowedMean;
+import arc.struct.ObjectMap;
+import mindustry.type.Sector;
+import mindustry.type.UnitType;
+import sc.type.UnitStack;
+
+public class UnitInfo {
+  private static final int valueWindow = 60;
+  public final Sector sector;
+  public final int id;
+  public static final UnitInfo[] all = new UnitInfo[10000];
+  public ObjectMap<UnitType, ExportStat> export = new ObjectMap<>();
+  public ObjectMap<UnitType, ExportStat> imports = new ObjectMap<>();
+
+  public UnitInfo(int id, Sector sector) {
+    this.sector = sector;
+    this.id = id;
+    all[id] = this;
+  }
+
+  public void handUnitsExport(UnitStack stack) {
+    handUnitsExport(stack.unit, stack.amount);
+  }
+
+  public void handUnitsExport(UnitType unit, int amount) {
+    export.get(unit, ExportStat::new).counter += amount;
+  }
+
+  public void handUnitsImport(UnitType unit, int amount) {
+    imports.get(unit, ExportStat::new).counter += amount;
+  }
+
+  public void handUnitsImport(UnitStack stack) {
+    handUnitsImport(stack.unit, stack.amount);
+  }
+
+  public static UnitInfo get(Sector targetSector) {
+    for (UnitInfo info : all) {
+      if (info == null) {
+        continue;
+      }
+      if (info.sector.equals(targetSector)) {
+        return info;
+      }
+    }
+    return null;
+  }
+
+  public static class ExportStat {
+    public transient float counter;
+    public transient WindowedMean means = new WindowedMean(valueWindow);
+    public transient boolean loaded;
+
+    public float mean;
+
+    public String toString() {
+      return mean + "";
+    }
+  }
+}
--- src/sc/world/blocks/payloads/UnitLanuchPad.java
+++ src/sc/world/blocks/payloads/UnitLanuchPad.java
@@ -1,90 +1,49 @@
 package sc.world.blocks.payloads;
 
 import arc.Core;
-import arc.Graphics.Cursor;
-import arc.Graphics.Cursor.SystemCursor;
-import arc.audio.Sound;
-import arc.graphics.Color;
-import arc.graphics.g2d.Draw;
 import arc.graphics.g2d.TextureRegion;
-import arc.math.Mathf;
-import arc.math.geom.Geometry;
 import arc.scene.ui.layout.Table;
-import arc.struct.EnumSet;
-import arc.struct.ObjectMap;
+import arc.struct.Seq;
 import arc.util.Log;
-import arc.util.Nullable;
 import arc.util.io.Reads;
 import arc.util.io.Writes;
+import mindustry.Vars;
+import mindustry.ctype.UnlockableContent;
 import mindustry.gen.Building;
-import mindustry.gen.Icon;
-import mindustry.gen.Player;
-import mindustry.gen.Sounds;
 import mindustry.gen.Unit;
-import mindustry.graphics.Drawf;
 import mindustry.graphics.Pal;
-import mindustry.logic.LAccess;
-import mindustry.type.Item;
-import mindustry.type.Liquid;
-import mindustry.type.Sector;
 import mindustry.type.UnitType;
 import mindustry.ui.Bar;
-import mindustry.ui.Styles;
+import mindustry.world.blocks.ItemSelection;
 import mindustry.world.blocks.payloads.Payload;
 import mindustry.world.blocks.payloads.PayloadBlock;
-import mindustry.world.meta.BlockFlag;
-import mindustry.world.meta.Stat;
-import mindustry.world.meta.StatUnit;
+import mindustry.world.blocks.payloads.UnitPayload;
 import sc.game.WaitTime;
-
-import static mindustry.Vars.*;
+import sc.util.Logs;
 
 /**
  * UnitLanuchPad
  */
 public class UnitLanuchPad extends PayloadBlock {
-  public float launchTime = 1f;
-  public float payloadsCapacity = 150;
-  public Sound launchSound = Sounds.none;
-  public TextureRegion lightRegion;
-  public TextureRegion podRegion;
-  public Color lightColor = Color.valueOf("eab678");
-  public float lightStep = 1f;
-  public int lightSteps = 3;
-  public float liquidPad = 2f;
-  public @Nullable Liquid drawLiquid;
-  public Color bottomColor = Pal.darkerMetal;
+  public float payloadsCapacity = 100f;
+  public float reload = 300f;
 
   public UnitLanuchPad(String name) {
     super(name);
-    this.solid = true;
     this.update = true;
-    this.configurable = true;
+    this.solid = true;
     this.hasItems = false;
-    // this.outputsPayload = true;
-    this.flags = EnumSet.of(BlockFlag.launchPad);
-    this.acceptsUnitPayloads = true;
-    this.rotate = false;
-    this.payloadSpeed = 1.2f;
+    this.configurable = true;
     this.clipSize = 120;
-  }
-
-  @Override
-  public void setStats() {
-    super.setStats();
-    stats.add(Stat.launchTime, launchTime / 60f, StatUnit.seconds);
-  }
-
-  @Override
-  public boolean outputsItems() {
-    return false;
-  }
-
-  @Override
-  public void load() {
-    this.lightRegion = Core.atlas.find(this.name + "-light");
-    this.podRegion = Core.atlas.find(this.name + "-pod");
-    super.load();
+    this.payloadSpeed = 1.2f;
+    this.outputsPayload = false;
+    this.acceptsPayload = true;
+    config(UnitType.class, (UnitLanuchPadBuild build, UnitType unit) -> {
+      if (canProduce(unit) && build.unit != unit) {
+        build.unit = unit;
+        build.payload = null;
+      }
+    });
   }
 
   @Override
@@ -92,163 +51,93 @@ public class UnitLanuchPad extends PayloadBlock {
     super.setBars();
     addBar("payloadsCapacity",
         (UnitLanuchPadBuild build) -> new Bar(() -> Core.bundle.format("bar.payloadsCapacity", build.payloadsTotal),
-            () -> Pal.items, () -> (float) build.payloadsTotal / payloadsCapacity));
-    addBar("progress", (UnitLanuchPadBuild build) -> new Bar(() -> Core.bundle.get("bar.launchcooldown"),
-        () -> Pal.ammo, () -> Mathf.clamp(build.launchCounter / launchTime)));
+            () -> Pal.items, () -> (float) (build.payloadsTotal / this.payloadsCapacity)));
   }
 
-  public static class UnitCount {
-    public UnitType unittype;
-    public int amount;
+  public boolean canProduce(UnitType t) {
+    return !t.isHidden() && !t.isBanned() && t.supportsEnv(Vars.state.rules.env);
+  }
 
-    public UnitCount(UnitType unit, int amount) {
-      this.unittype = unit;
-      this.amount = amount;
-    }
+  @Override
+  public TextureRegion[] icons() {
+    return new TextureRegion[] { region, topRegion };
   }
 
   public class UnitLanuchPadBuild extends PayloadBlockBuild<Payload> {
+    public UnitType unit;
     public float payloadsTotal;
-    public float launchCounter;
-    public ObjectMap<String, UnitCount> unitmap = new ObjectMap<>();
+    public Seq<UnitType> unitTypes = new Seq<>();
+    public float timer = 0f;
+
+    @Override
+    public boolean acceptPayload(Building source, Payload payload) {
+      if (payload instanceof UnitPayload) {
+        /**
+         * Log.info("输入单位UnitType" + ((UnitPayload) payload).unit.type());
+         * Log.info("输入单位Unit" + ((UnitPayload) payload).unit);
+         * Log.info("选择单位UnitType" + this.unit);
+         */
+        if (((UnitPayload) payload).unit.type() == this.unit
+            && payloadsTotal + ((UnitPayload) payload).unit.hitSize <= payloadsCapacity) {
+          payloadsTotal += ((UnitPayload) payload).unit.hitSize;
+          unitTypes.add(((UnitPayload) payload).unit.type());
+          return true;
+        } else {
+          return false;
+        }
+      } else {
+        return false;
+      }
+    }
 
     @Override
     public void updateTile() {
-      boolean b = WaitTime.waittime(120);
+      timer++;
+      if (timer >= reload) {
+        int u = 0;
+        for (var l : unitTypes) {
+          Log.info(u + " " + l);
+          u++;
+        }
+        clear();
+      }
+      boolean b = WaitTime.waittime(120f);
+      if (b) {
+      }
       super.updateTile();
       if (moveInPayload(false) && efficiency > 0) {
-        UnitType payloadUnit = (UnitType) payload;
-        addUnit((UnitType) payload);
         payload = null;
-        payloadsTotal += payloadUnit.hitSize;
-      }
-      if (b) {
-        for (var unit : unitmap.entries()) {
-          Log.info("单位种类 " + unit.key);
-          Log.info("单位数量 " + unit.value.amount);
-        }
-
       }
     }
 
-    public void addUnit(UnitType unittype) {
-      for (var unit : unitmap.entries()) {
-        if (unittype.name == unit.key) {
-          unit.value.amount++;
-          return;
-        }
-      }
-      unitmap.put(unittype.name, new UnitCount(unittype, 1));
+    public void clear() {
+      unitTypes.clear();
+      timer = 0f;
+      payloadsTotal = 0f;
     }
 
     @Override
-    public Cursor getCursor() {
-      return !state.isCampaign() || net.client() ? SystemCursor.arrow : super.getCursor();
-    }
-
-    @Override
-    public boolean shouldConsume() {
-      return launchCounter < launchTime;
-    }
-
-    @Override
-    public boolean acceptUnitPayload(Unit unit) {
-      return true;// payloadsCapacity - payloadsTotal >= unit.type.hitSize;
-    }
-
-    @Override
-    public double sense(LAccess sensor) {
-      if (sensor == LAccess.progress)
-        return Mathf.clamp(launchCounter / launchTime);
-      return super.sense(sensor);
-    }
-
-    @Override
-    public void draw() {
-      super.draw();
-      if (lightRegion.found()) {
-        Draw.color(lightColor);
-        float progress = Math.min((float) items.total() / itemCapacity, launchCounter / launchTime);
-        for (int i = 0; i < 4; i++) {
-          for (int j = 0; j < lightSteps; j++) {
-            float alpha = Mathf.curve(progress, (float) j / lightSteps, (j + 1f) / lightSteps);
-            float offset = -(j - 1f) * lightStep;
-            Draw.color(Pal.metalGrayDark, lightColor, alpha);
-            Draw.rect(lightRegion, x + Geometry.d8edge(i).x * offset, y + Geometry.d8edge(i).y * offset, i * 90);
-          }
-        }
-        Draw.reset();
-      }
-      Drawf.shadow(x, y, size * tilesize);
-      Draw.rect(podRegion, x, y);
-
-      Draw.reset();
-    }
-
-    @Override
-    public boolean acceptItem(Building source, Item item) {
-      return false;
-    }
-
-    @Override
-    public void display(Table table) {
-      super.display(table);
-
-      if (!state.isCampaign() || net.client() || team != player.team())
-        return;
-
-      table.row();
-      table.label(() -> {
-        Sector dest = state.rules.sector == null ? null : state.rules.sector.info.destination;
-
-        return Core.bundle.format("launch.destination",
-            dest == null || !dest.hasBase() ? Core.bundle.get("sectors.nonelaunch") : "[accent]" + dest.name());
-      }).pad(4).wrap().width(200f).left();
-    }
-
-    @Override
-    public boolean shouldShowConfigure(Player player) {
-      return state.isCampaign();
+    public UnitType config() {
+      return unit;
     }
 
     @Override
     public void buildConfiguration(Table table) {
-      if (!state.isCampaign() || net.client()) {
-        deselect();
-        return;
-      }
-
-      table.button(Icon.upOpen, Styles.cleari, () -> {
-        ui.planet.showSelect(state.rules.sector, other -> {
-          if (state.isCampaign() && other.planet == state.rules.sector.planet) {
-            var prev = state.rules.sector.info.destination;
-            state.rules.sector.info.destination = other;
-            if (prev != null) {
-              prev.info.refreshImportRates(state.getPlanet());
-            }
-          }
-        });
-        deselect();
-      }).size(40f);
-    }
-
-    @Override
-    public byte version() {
-      return 1;
+      ItemSelection.buildTable(UnitLanuchPad.this, table,
+          Vars.content.units().select(UnitLanuchPad.this::canProduce).as(),
+          () -> (UnlockableContent) config(), this::configure, selectionRows, selectionColumns);
     }
 
     @Override
     public void write(Writes write) {
       super.write(write);
-      write.f(launchCounter);
+      write.s(unit == null ? -1 : unit.id);
     }
 
     @Override
     public void read(Reads read, byte revision) {
       super.read(read, revision);
-      if (revision >= 1) {
-        launchCounter = read.f();
-      }
+      unit = Vars.content.unit(read.s());
     }
   }
 }
